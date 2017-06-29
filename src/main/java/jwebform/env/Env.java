667ec21bea8af9723a10140ffb81aa7d3454356a
Jochen Pier <jpier@me.com>
2017-06-29 23:13:10 +0200

    Add optional session to env

--- src/main/java/jwebform/env/Env.java
+++ src/main/java/jwebform/env/Env.java
@@ -3,12 +3,22 @@ package jwebform.env;
 // holds pointers to the web-env. (request, response, session, maybe headers...)
 public class Env {
 	Request request;
+	SessionGet sessionGet;
+	SessionSet sessionSet;
 
 	public Env(Request request) {
 		this.request = request;
 	}
 
+	public Env(Request request, SessionGet sessionGet, SessionSet sessionSet) {
+		this(request);
+		this.sessionGet = sessionGet;
+		this.sessionSet = sessionSet;
+	}
+
+	
 	public Request getRequest() {
 		return request;
 	}
+	
 }
