e5a183fd9ef384ab0f54a76acd9aa3370e45b52d
Jochen Pier <jpier@me.com>
2017-06-29 23:11:47 +0200

    add idea to simplyfy interface

--- src/main/java/jwebform/element/structure/Element.java
+++ src/main/java/jwebform/element/structure/Element.java
@@ -4,6 +4,7 @@ import jwebform.validation.ValidationResult;
 
 public interface Element {
 
+	// RFE: Introduce "RenderHints" Object with tabIndex, MessageSource, additionalAttributes
 	public String getHtml(int tabIndex, ValidationResult overridenValidationResult);
 	
 }
