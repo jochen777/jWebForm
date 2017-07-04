4e0d965c39379f6bc34baa130295f0f4429dadb6
Jochen Pier <jpier@me.com>
2017-07-05 00:09:48 +0200

    cleanup

--- src/main/java/jwebform/validation/FormValidator.java
+++ src/main/java/jwebform/validation/FormValidator.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import jwebform.element.structure.Element;
+import jwebform.env.Env;
 
 // Validates a complete Form
 @FunctionalInterface
@@ -11,6 +12,7 @@ public interface FormValidator {
 	/**
 	 * checks a complete form. 
 	 * If something is invalid, associate the validationResult to this element in the resulting map 
+	 * @param env 
 	 * @param form
 	 * @return
 	 */
