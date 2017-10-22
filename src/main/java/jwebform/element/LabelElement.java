0047a1f9097170fd00baecee34de568b7bcc7b93
Jochen Pier <jpier@me.com>
2017-10-22 22:39:18 +0200

    label field

---
+++ src/main/java/jwebform/element/LabelElement.java
@@ -0,0 +1,28 @@
+package jwebform.element;
+
+import jwebform.element.structure.Element;
+import jwebform.element.structure.ElementResult;
+import jwebform.env.Env.EnvWithSubmitInfo;
+
+public class LabelElement implements Element {
+
+	public static String KEY = "jwebform.element.LabelInput"; 
+	
+	public final String label;
+	
+  public LabelElement(String label) {
+	  this.label = label;
+  }
+  
+	
+  @Override
+  public ElementResult prepare(EnvWithSubmitInfo env) {
+    return new ElementResult("_label", t -> "<label>" + label + "</label>", KEY, this);
+  }
+
+
+public String getLabel() {
+	return label;
+}
+
+}
