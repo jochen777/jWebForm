9d58146851a953801c519eff2107838577ac126b
Jochen Pier <jpier@me.com>
2017-10-22 23:31:49 +0200

    password input

---
+++ src/main/java/jwebform/element/PasswordInput.java
@@ -0,0 +1,36 @@
+package jwebform.element;
+
+import jwebform.element.structure.Element;
+import jwebform.element.structure.HTMLProducer;
+import jwebform.element.structure.OneFieldDecoration;
+import jwebform.element.structure.StandardElementRenderer;
+import jwebform.validation.Validator;
+import jwebform.view.Tag;
+
+public class PasswordInput extends TextInput implements Element {
+
+  public final static String KEY = "jwebform.element.PasswordInput";
+  
+
+  public PasswordInput(String name, OneFieldDecoration decoration, /* you don't want an initial value! */
+      Validator validator) {
+	  super(name, decoration, "", validator);
+  }
+  
+  @Override
+  protected HTMLProducer getDefault() {
+    return producerInfos -> {
+      StandardElementRenderer renderer = new StandardElementRenderer();
+      String errorMessage = renderer.generateErrorMessage(producerInfos);
+      Tag inputTag = renderer.generateInputTag(producerInfos, "password", "input");
+      String html = decoration.getLabel() + errorMessage + inputTag.getStartHtml();
+      return html;
+    };
+  }
+  
+  @Override
+  public String toString() {
+    return String.format("PasswordInput. name=%s", name);
+  }
+
+}
