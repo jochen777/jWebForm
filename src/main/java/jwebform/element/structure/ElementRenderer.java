097c942f32d485db02ae40eabe30047291234caf
Jochen Pier <jpier@me.com>
2017-11-27 22:07:45 +0100

    add documentation

--- src/main/java/jwebform/element/structure/ElementRenderer.java
+++ src/main/java/jwebform/element/structure/ElementRenderer.java
@@ -2,27 +2,31 @@ package jwebform.element.structure;
 
 public interface ElementRenderer {
 
+  // Renders a single input-element including label and help
   String renderInput(
       String type,
       ProducerInfos pi,
       OneFieldDecoration decoration,
       String additional);
 
+  // renders an input value, that is not a typical input (for example TextArea)
   String renderInputComplex(
       String tagname,
       String inBetweenHtml,
       ProducerInfos pi,
       OneFieldDecoration decoration);
 
+  // just renders label and help, provide the input-element yourself
   String renderInputFree(
       String free,
       ProducerInfos pi,
       OneFieldDecoration decoration,
       InputVariant variant);
 
-
+  // render the aria attribute
   String renderAriaDescribedBy(ProducerInfos pi, OneFieldDecoration decoration);
 
+  // render the value attribute
   String renderValue(String value);
 
   // This may be used, to render label for radio-inputs. No error-highlighting, no required
