0b07bf2bb83e4a2b88b89483b36b91b3bf27ff7c
Jochen Pier <jpier@me.com>
2017-12-11 23:36:29 +0100

    submit with optional name

--- src/main/java/jwebform/element/SubmitType.java
+++ src/main/java/jwebform/element/SubmitType.java
@@ -10,19 +10,26 @@ public class SubmitType implements Element {
   public static String KEY = "jwebform.element.SubmitButton";
 
   public final String label;
+  public final String name;
 
   public SubmitType(String label) {
-    this.label = label;
+    this(label, "submit");
   }
 
+  public SubmitType(String label, String name) {
+    this.label = label;
+    this.name = name;
+  }
+
+  
   @Override
   public ElementResult apply(EnvWithSubmitInfo env) {
-    return new ElementResult("submit", getDefault(), SubmitType.KEY);
+    return new ElementResult(name, getDefault(), SubmitType.KEY);
   }
 
   public HTMLProducer getDefault() {
     return producerInfos -> "<input tabindex=\"" + producerInfos.getTabIndex()
-        + "\" type=\"submit\" value=\"" + label + "\">\n";
+        + "\" type=\"submit\" name=\""+ name + "\" value=\"" + label + "\">\n";
   }
 
 }
