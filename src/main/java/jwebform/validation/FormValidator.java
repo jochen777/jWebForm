cdd316aef2fdcbc5df1d86c15af07083c34fd241
Jochen Pier <jpier@me.com>
2018-08-18 01:05:20 +0200

    Release specifics

--- src/main/java/jwebform/validation/FormValidator.java
+++ src/main/java/jwebform/validation/FormValidator.java
@@ -6,10 +6,13 @@ import jwebform.processors.ElementValdationResults;
 // Validates a complete Form
 @FunctionalInterface
 public interface FormValidator {
+
   /**
    * checks a complete form. If something is invalid, associate the validationResult to this element
    * in the resulting map
    * 
+   * @param elements Elements of the form to validate
+   * @return validation results for the elements
    */
   ElementValdationResults validate(ElementResults elements);
 }
