5cf5a427e2fd6e9cf7daba81160cbd7e2a100291
Jochen Pier <jochen.pier@giz.de>
2021-08-24 08:52:58 +0200

    Correct template, beautify comments

--- jwebform/src/main/java/jwebform/validation/criteria/StrongPassword.java
+++ jwebform/src/main/java/jwebform/validation/criteria/StrongPassword.java
@@ -3,10 +3,14 @@ package jwebform.validation.criteria;
 // See: http://stackoverflow.com/questions/3802192/regexp-java-for-password-validation
 /*
  * 
- * ^ # start-of-string (?=.*[0-9]) # a digit must occur at least once (?=.*[a-z]) # a lower case
- * letter must occur at least once (?=.*[A-Z]) # an upper case letter must occur at least once
- * (?=.*[@#$%^&+=]) # a special character must occur at least once (?=\S+$) # no whitespace allowed
- * in the entire string .{8,} # anything, at least eight places though $ # end-of-string
+ * # start-of-string (?=.*[0-9]) 
+ * # a digit must occur at least once (?=.*[a-z]) 
+ * # a lower case letter must occur at least once (?=.*[A-Z]) 
+ * # an upper case letter must occur at least once (?=.*[@#$%^&+=]) 
+ * # a special character must occur at least once (?=\S+$) 
+ * # no whitespace allowed in the entire string .{8,} 
+ * # anything, at least eight places though $ 
+ * # end-of-string
  * 
  */
 public class StrongPassword extends Regex {
